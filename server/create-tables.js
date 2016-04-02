607c9790e879d06cac085c3eaca6dc43f99cbad6
System Administrator <root@192.168.2.4>
2016-04-02 10:29:58 +0200

    added custom api explorer layout

---
+++ server/create-tables.js
@@ -0,0 +1,8 @@
+var server = require('./server');
+var ds = server.dataSources.mysqlDs;
+var lbTables = ['AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
