e031415336d5208067c34aee871c95804c4f2d63
System Administrator <root@192.168.2.4>
2016-04-02 16:20:47 +0200

    datasources fix

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,1 +1,14 @@
-module.exports = require('./datasources.acceptation.js');
+var parseDbUrl = require("parse-database-url");
+var dbConfig = parseDbUrl(process.env.JAWSDB_URL);
+console.log(dbConfig);
+module.exports = {
+  mysqlDs: {
+    name: "mysqlDs",
+    connector: dbConfig.driver,
+    hostname: dbConfig.host,
+    port: dbConfig.port || 3306,
+    user: dbConfig.user,
+    password: dbConfig.pass,
+    database: dbConfig.database
+  }
+};
--- server/datasources.production.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./datasources.acceptation.js');
