6f97a36b581eae95ef3b96cf188eb5b0e0db1c0d
System Administrator <root@192.168.2.4>
2016-04-03 12:59:19 +0200

    fix

--- server/boot/init-data.js
+++ server/boot/init-data.js
@@ -99,7 +99,7 @@ module.exports = function(app) {
 
 	async.series([
 		//	function(cb){ destroyData(); cb();  },
-			function(cb){ createSchema(); cb(); },
+//			function(cb){ createSchema(); cb(); },
       function(cb){ createTags(3); cb(); },
       function(cb){ createAuthors(3); cb(); },
       function(cb){ createPosts(3); cb(); },
