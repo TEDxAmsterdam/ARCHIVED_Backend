0216c2f63d0f99e63c6ff12c695207e0e326d75a
System Administrator <dligthart@gmail.com>
2016-04-14 07:38:31 +0200

    added linkedin authentication

---
+++ client/html/script.js
@@ -0,0 +1,14 @@
+
+OAuth.initialize('qrEyny_6uTaBqjNsd2EEjl79hAg');
+//var apiEndpoint='https://api-acc-tedx-amsterdam.herokuapp.com';
+/*var apiEndpoint = '';
+$.get(apiEndpoint + '/oauth/token', function( data ) {
+  console.log('data', data);
+   OAuth.popup('linkedin', {
+    state: data.token
+  }).done(function(result) {
+    console.log('result', result);
+    $.post(apiEndpoint + '/auth', {code: result.code})
+  });
+});
+*/
--- server/boot/scripts.js
+++ server/boot/scripts.js
@@ -2,7 +2,19 @@ var OAuth = require('oauthio');
 
 module.exports = function(app) {
 
-	OAuth.initialize(app.get('oauth').key, app.get('oauth').secret); 
+	OAuth.initialize(app.get('oauth').key, app.get('oauth').secret);
+
+	app.get('/signin', OAuth.auth('linkedin', 'http://localhost:4001/oauth/redirect'));
+
+	app.get('/oauth/redirect', OAuth.redirect(function(result, req, res) {
+	    if (result instanceof Error) {
+	        res.send(500, "error: " + result.message);
+	    }
+	    result.me().done(function(me) {
+	        console.log(me);
+	        res.send(200, JSON.stringify(me));
+	    });
+	}));
 
 	app.get('/oauth/token', function(req, res, next) {
 		var token = OAuth.generateStateToken(req);
@@ -10,14 +22,38 @@ module.exports = function(app) {
 		next();
 	});
 
-	app.get('/signin', function(req, res, next) {
-	  OAuth.auth('linkedin', req.session, {
-	    code: JSON.parse(req.body).code
-	  }).then(function(oauthResult) {
-			console.log('authResult', oauthResult);
-	    //todo with oauthResult
-	    //oauthResult.access_token oauthResult.refresh_token
+	app.post('/auth', function(req, res, next) {
+
+		var code =  req.query.code;
+
+		console.log('code', code, req.session);
+
+		if(null === code) {
+			res.send(400, 'An error occured');
 			next();
-	  });
+		}
+
+  	OAuth.auth('linkedin', req.session, {
+        code: code
+    })
+    .then(function (request_object) {
+				return request_object.get('/me');
+    })
+    .then(function (info) {
+        var user = {
+            email: info.email,
+            firstname: info.first_name,
+            lastname: info.last_name
+        };
+        //login your user here.
+        res.status(200).send('Successfully logged in');
+				next();
+    })
+    .fail(function (e) {
+        //handle errors here
+        res.status(400).send('An error occured');
+				next();
+    });
+
 	});
 };
